efbe9d7c047498b6985f90e98aae993345f89c3c
panda <p4nd4@posteo.de>
2023-11-01 10:48:33 +0100

    mail server: receive POST requests on 8080

---
+++ server.ts
@@ -0,0 +1,22 @@
+const server = Deno.listen({ port: 8080});
+
+for await (const conn of server) {
+  (async () => {
+    const httpConn = Deno.serveHttp(conn);
+    for await (const requestEvent of httpConn) {
+      // ... handle requestEvent ...
+			if (requestEvent.request.method == "POST") {
+  			console.log(requestEvent);
+  			//const requestBody = await requestEvent.request.formData();
+  			const requestBody = await requestEvent.request.text();
+				console.log(requestBody);
+				const params = new URLSearchParams(requestBody);
+				console.log(params);
+				console.log(params.get('fullname'))
+			  requestEvent.respondWith(new Response("Dings\n"));
+			} else {
+				requestEvent.respondWith(new Response("nein\n"))
+			}
+    }
+  })();
+}
